1581a6c5c8025726a019ca3f439a9b098d478dd2
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-21 21:59:24 -0500

    Setup gallery components

---
+++ app/javascript/components/Galleries/CostaRica.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const CostaRica = () => {
+  return (
+    <div>
+      <h1>Costa Rica</h1>
+    </div>
+  )
+}
+
+export default CostaRica
---
+++ app/javascript/components/Galleries/Northeast.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Northeast = () => {
+  return (
+    <div>
+      <h1>Northeast</h1>
+    </div>
+  )
+}
+
+export default Northeast
