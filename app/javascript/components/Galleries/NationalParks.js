d1650b2a95fd81e8c3fb5b2b2d4889955d33b6f4
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-23 16:35:40 -0500

    Added national park gallery

--- app/javascript/components/Galleries/NationalParks.js
+++ app/javascript/components/Galleries/NationalParks.js
@@ -1,9 +1,36 @@
 import React from "react";
 
+import ImageTile from "../Utilities/ImageTile"
+
 export const NationalParks = () => {
+  let images = []
+
+  for (let i = 1; i < 67; i++) {
+    let image = {
+      id: i,
+      name: "national-parks",
+      fullSize: `https://perpetually-lost-prod.s3.amazonaws.com/national-parks/FullSize/nationalparks-${i}.jpg`,
+      thumbnail: `https://perpetually-lost-prod.s3.amazonaws.com/national-parks/Thumbnails/nationalparks-thumbnail-${i}.jpg`
+    }
+    images.push(image)
+  }
+
+  let imageTiles = images.map((image) => {
+    return (
+      <ImageTile
+        key={image.id}
+        id={image.id}
+        name={image.name}
+        fullSize={image.fullSize}
+        thumbnail={image.thumbnail}
+      />
+    );
+  });
   return (
-    <div>
-      <h1>National Parks</h1>
+    <div className="container is-widescreen">
+      <div className="images-container"> 
+        {imageTiles}
+      </div>  
     </div>
   )
 }
