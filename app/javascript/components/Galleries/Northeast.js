caaa2d86d8db2065e0868c651705496f265b2291
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-23 16:49:11 -0500

    Added northeast gallery

--- app/javascript/components/Galleries/Northeast.js
+++ app/javascript/components/Galleries/Northeast.js
@@ -1,9 +1,36 @@
 import React from "react";
 
+import ImageTile from "../Utilities/ImageTile"
+
 export const Northeast = () => {
+  let images = []
+
+  for (let i = 1; i < 33; i++) {
+    let image = {
+      id: i,
+      name: "northeast",
+      fullSize: `https://perpetually-lost-prod.s3.amazonaws.com/northeast/FullSize/northeast-${i}.jpg`,
+      thumbnail: `https://perpetually-lost-prod.s3.amazonaws.com/northeast/Thumbnails/northeast-thumbnail-${i}.jpg`
+    }
+    images.push(image)
+  }
+
+  let imageTiles = images.map((image) => {
+    return (
+      <ImageTile
+        key={image.id}
+        id={image.id}
+        name={image.name}
+        fullSize={image.fullSize}
+        thumbnail={image.thumbnail}
+      />
+    );
+  });
   return (
-    <div>
-      <h1>Northeast</h1>
+    <div className="container is-widescreen">
+      <div className="images-container"> 
+        {imageTiles}
+      </div>  
     </div>
   )
 }
