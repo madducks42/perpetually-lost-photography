9971d4368ad063d9cef548dfb0ade2eb4fcda353
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-23 19:23:20 -0500

    updated route for custom login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, path: 'admin', path_names: { sign_in: 'login' }
+
   root 'homepages#index'
 
   get '/costa-rica', to: 'homepages#index'
