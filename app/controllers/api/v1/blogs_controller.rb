2cbf4954f5b3f2bbdc53f33b3bfb612e6c58b727
Maddox Grey <maddoxgrey42@gmail.com>
2021-02-24 22:10:32 -0500

    Finished setting up new post feature

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -10,7 +10,30 @@ class Api::V1::BlogsController < ApiController
   def show
     render json: Blog.find(params[:id]), serializer: BlogShowSerializer
   end
+
+  # CREATE /blogs/new
+  def create
+    post = Blog.new(blog_params)
+    post.user = current_user
+
+    if post.save
+      render json: post
+    else
+      render json: { errors: post.errors.full_messages }
+    end 
+
+  end
   
-  
+  protected
+
+  def blog_params
+    params.require(:blog).permit([:title, :caption, :body, :image, :category])
+  end
+
+  def authorize_user
+    if !user_signed_in? || current_user.role != "admin"
+      redirect_to root_path
+    end
+  end
 
 end
